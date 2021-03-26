b5536515bd64e89b94f33724581424d944a3479f
Steve Emerson <sbemerson@outlook.com>
2021-03-26 05:06:52 -0400

    Cat and Search Finished

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Input} from 'reactstrap';
  
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
 
- function searchFunction() {
- }
+class SearchIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+      term: ""
+    }
+  }
+
+  searchFunction = (e) => {
+    this.setState({term: e.target.value});
+  }
 
  render() {
+  return(
      <div>
-       <Input placeholder='Search Here' />
+       <Input placeholder='Search Here' onChange={(e) => this.searchFunction(e)}/>
        <h3>Results:</h3>
+       {this.state.things.filter((thing) => thing.startsWith(this.state.term)).map(item => <p>{item}</p>)}
      </div>
+  )
  }
+}
 
  
-export SearchIndex;
+export default SearchIndex;
